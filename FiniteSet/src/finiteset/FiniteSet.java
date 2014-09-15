8c8f4f4d29a6d2c8f8aaa16115ab3f7d41c20fc2
benslaw <beslaw@vassar.edu>
2014-09-15 13:11:31 -0400

    Complete Redo -- Update 2
    Now with BSTs!

--- FiniteSet/src/finiteset/FiniteSet.java
+++ FiniteSet/src/finiteset/FiniteSet.java
@@ -12,7 +12,7 @@ import java.util.*;
  */
 public class FiniteSet {
 
-    interface Listy {
+    interface BST {
         
         public int cardinality();
         
@@ -20,17 +20,17 @@ public class FiniteSet {
         
         public boolean member( int thing );
         
-        public Listy add( int thing );
+        public BST add( int thing );
         
-        public Listy remove( int thing );
+        public BST remove( int thing );
         
-        public Listy union( Listy set );
+        public BST union( BST set );
         
-        public Listy inter( Listy set );
+        public BST inter( BST set );
         
     }
     
-    static class Empty implements Listy {
+    static class Empty implements BST {
         
         Empty() { }
         
@@ -46,36 +46,42 @@ public class FiniteSet {
             return false;
         }
         
-        public Listy add( int thing ) {
-            return new notEmpty(thing,new Empty());
+        public BST add( int thing ) {
+            return new notEmpty(thing,new Empty(), new Empty());
         }
         
-        public Listy remove( int thing ) {
+        public BST remove( int thing ) {
             return new Empty();
         }
         
-        public Listy union(Listy set) {
+        public BST union(BST set) {
             return set;
         }
         
-        public Listy inter(Listy set) {
+        public BST inter(BST set) {
             return new Empty();
         }
         
     }
     
-    static class notEmpty implements Listy {
+    static class notEmpty implements BST {
         
-        int element;
-        Listy restList;
+        int here;
+        BST lefty;
+        BST righty;
         
-        notEmpty ( int element, Listy restList) {
-            this.element = element;
-            this.restList = restList;
+        notEmpty ( int here, BST lefty, BST righty) {
+            this.here = here;
+            this.lefty = lefty;
+            this.righty = righty;
+        }
+        
+        public BST empty() {
+            return new Empty();
         }
         
         public int cardinality() {
-            return 1 + restList.cardinality();
+            return 1 + lefty.cardinality() + righty.cardinality();
         }
         
         public boolean isEmptyHuh() {
@@ -83,35 +89,40 @@ public class FiniteSet {
         }
         
         public boolean member( int thing ) {
-            return (element == thing || restList.member(thing));
+            return (here == thing || lefty.member(thing) 
+                    || righty.member(thing));
         }
         
-        public Listy add(int thing) {
-            if (this.member(thing)) {
+        public BST add(int thing) {
+            if (this.here == thing) {
                 return this;
+            } else if(thing < this.here) {
+                return new notEmpty(this.here, this.lefty.add(thing), this.righty);
             } else {
-                return new notEmpty(thing, this);
+                return new notEmpty(this.here, this.lefty, this.righty.add(thing));
             }
         }
         
-        public Listy remove(int thing) {
-            if (this.element != thing) {
-                return new notEmpty(this.element, this.restList.remove(thing));
+        public BST remove(int thing) {
+            if (thing == this.here) {
+                return this.lefty.union(this.righty);
+            } else if (thing < this.here) {
+                return new notEmpty(this.here, this.lefty.remove(thing), 
+                        this.righty);
             } else {
-                return this.restList;
+                return new notEmpty(this.here, this.lefty, 
+                        this.righty.remove(thing));
             }
         }
         
-        public Listy union(Listy set) {
-            if(this.isEmptyHuh()) {
-                return set;
-            } else {
-                return this.restList.union(set.add(this.element));
-            }
+        public BST union(BST set) {
+            return this.lefty.union(set.union(this.righty).add(this.here));
         }
         
-        public Listy inter(Listy set) {
-            return set;
+        public BST inter(BST set) {
+            if(set.member(here)) {
+                
+            }
         }
         
     }
@@ -122,15 +133,16 @@ public class FiniteSet {
         return rand.nextInt((max - min) + 1) + min;
     }
     
-    public static Listy randomListy( int lengthy ) {
+    public static BST randomBST( int lengthy ) {
         if(lengthy == 0) {
             return new Empty();
         } else {
-            return new notEmpty(randomInt(0,10),randomListy(lengthy - 1));
+            return new notEmpty(randomInt(0,10),randomBST(lengthy - 1),
+                    randomBST(lengthy - 1));
         }
     }
     
-    public static boolean check_union( Listy set1, Listy set2 ) {
+    public static boolean check_union( BST set1, BST set2 ) {
         int elt = randomInt(0,100);
         if ((set1.add(elt)).union(set2).member(elt) &&
                 set1.union(set2.add(elt)).member(elt)) {
@@ -142,13 +154,13 @@ public class FiniteSet {
 
     public static void main(String[] args) {
         
-        Listy mt = new Empty();
-        Listy not_mt5 = new notEmpty(5, (new Empty()));
+        BST mt = new Empty();
+        BST not_mt5 = new notEmpty(5, (new Empty()), (new Empty()));
         for(int i = 0; i < 10; i++) {
             int lengthy = randomInt(0,10);
             int lengthy2 = randomInt(0,10);
-            Listy x = randomListy(lengthy);
-            Listy y = randomListy(lengthy2);
+            BST x = randomBST(lengthy);
+            BST y = randomBST(lengthy2);
             System.out.println(check_union(x, y));
         }
         
